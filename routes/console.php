a346cf37cf4913064c8b4d661ce0d39fe760399b
joaoleonard <joao.leobm@gmail.com>
2025-05-23 09:02:39 -0300

    fix: Update daily reminder time to 09:00 in console schedule

--- routes/console.php
+++ routes/console.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::command('app:send-show-reminder')->timezone('America/Sao_Paulo')->dailyAt('07:00');
+Schedule::command('app:send-show-reminder')->timezone('America/Sao_Paulo')->dailyAt('09:00');
